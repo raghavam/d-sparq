bf6f3ab5b68a1bc309ec6b687a1585139207d6f6
Raghava <raghava@knoesis.org>
2014-04-22 20:08:38 -0400

    removed much of the older content in this class and modified it to generate digest messages for each term in the triple and push it to Mongo router.

--- src/dsparq/load/HashGeneratorMR.java
+++ src/dsparq/load/HashGeneratorMR.java
@@ -2,47 +2,39 @@ package dsparq.load;
 
 import java.io.IOException;
 import java.io.StringReader;
-import java.net.InetAddress;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileInputFormat;
-import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.KeyValueTextInputFormat;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.mapred.RunningJob;
 import org.apache.hadoop.mapred.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
+import org.semanticweb.yars.nx.Node;
+import org.semanticweb.yars.nx.parser.NxParser;
 
-import com.hp.hpl.jena.graph.Triple;
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 import com.mongodb.Mongo;
-import com.mongodb.MongoException;
-import com.mongodb.MongoOptions;
-import com.mongodb.ServerAddress;
-import com.mongodb.WriteConcern;
+import com.mongodb.MongoClient;
 
 import dsparq.misc.Constants;
-import dsparq.misc.HostInfo;
 import dsparq.misc.PropertyFileHandler;
 import dsparq.util.Util;
 
@@ -60,167 +52,105 @@ public class HashGeneratorMR extends Configured implements Tool {
 	private static class Map extends MapReduceBase implements 
 			Mapper<Text, Text, Text, Text> {
 		
-		// read Vertex ID of subject from DB.
-		private Mongo mongo;
-		private DBCollection idValCollection;
-		private DBCollection eidValCollection;
-		private Model model;
-
-		@Override
-		public void configure(JobConf jobConf) {
-			log.info("Configuring Mongo...");
-			try {
-				model = ModelFactory.createDefaultModel();
-				String hostName = InetAddress.getLocalHost().getHostName();
-				// hostName starts with nimbus. Extracting the number after "nimbus"
-				Pattern pattern = Pattern.compile("\\d+");
-				Matcher matcher = pattern.matcher(hostName);
-				matcher.find();
-				int mongosNum = Integer.parseInt(matcher.group());
-				int mongosCount = Integer.parseInt(jobConf.get("mongosCount"));
-				List<HostInfo> mongosList = new ArrayList<HostInfo>(mongosCount);
-				for(int i=0; i<mongosCount; i++) {
-					String[] hostPort = jobConf.get("mongos"+i).split(":");
-					mongosList.add(new HostInfo(hostPort[0], 
-									Integer.parseInt(hostPort[1])));
-				}
-				MongoOptions mongoOptions = new MongoOptions();
-				// default is 10. increased it to avoid "Connection refused" exception
-				mongoOptions.setConnectionsPerHost(20);
-				ServerAddress serverAddress = new ServerAddress(
-						mongosList.get(mongosNum%mongosCount).getHost(),
-						mongosList.get(mongosNum%mongosCount).getPort());
-				mongo = new Mongo(serverAddress, mongoOptions);
-				log.info("Configuration done (Mongos identified)");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			DB db = mongo.getDB(Constants.MONGO_RDF_DB);
-			idValCollection = db.getCollection(
-					Constants.MONGO_IDVAL_COLLECTION);
-			eidValCollection = db.getCollection(Constants.MONGO_EIDVAL_COLLECTION);
-		}
-		
 		@Override
 		public void map(Text key, Text value, 
 				OutputCollector<Text, Text> output,
 				Reporter reporter) throws IOException {
 			
 			// expected input format: subject predicate object
-			reporter.progress();
 			
 			StringReader reader = new StringReader(key.toString());
-			model.read(reader, Constants.BASE_URI, "N-TRIPLE");
-			StmtIterator stmtIterator = model.listStatements();
-			Triple triple = stmtIterator.next().asTriple();
-			reader.close();
-			model.removeAll();
-			// subject & object go into idValCollection
-			// predicate goes into eidValCollection
+			NxParser nxParser = new NxParser(reader);
+			Node[] nodes = nxParser.next();
+			StringBuilder strBuilder = new StringBuilder();
+			strBuilder.append(nodes[1].toString()).
+						append(Constants.TRIPLE_TERM_DELIMITER).
+						append(nodes[2].toString());
+			//group by subject
+			output.collect(new Text(nodes[0].toString()), 
+					new Text(strBuilder.toString()));
+		}
+	}
+	
+	private static class Reduce extends MapReduceBase implements
+	Reducer<Text, Text, Text, Text> {
+		
+		// read Vertex ID of subject from DB.
+				private Mongo mongo;
+				private DBCollection idValCollection;
+
+				@Override
+				public void configure(JobConf jobConf) {
+					log.info("Configuring Mongo...");
+					try {
+						String[] hostPort = jobConf.get("mongo.router").split(":");
+						mongo = new MongoClient(hostPort[0], 
+								Integer.parseInt(hostPort[1]));
+						DB db = mongo.getDB(Constants.MONGO_RDF_DB);
+						idValCollection = db.getCollection(
+								Constants.MONGO_IDVAL_COLLECTION);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+		
+		@Override
+		public void reduce(Text key, Iterator<Text> values,
+				OutputCollector<Text, Text> output, Reporter reporter) {
 			try {
-				String subject = triple.getSubject().toString();
-				String hashSub = Util.generateMessageDigest(subject);
+				List<DBObject> docs = new ArrayList<DBObject>();
+				String keyStr = key.toString();
+				String subjectDigestValue = Util.generateMessageDigest(keyStr);
 				BasicDBObject subDoc = new BasicDBObject();
-				subDoc.put(Constants.FIELD_STR_VALUE, subject);
-				subDoc.put(Constants.FIELD_HASH_VALUE, hashSub);
-				subDoc.put("isType", "false");
-				try {
-					idValCollection.insert(subDoc, WriteConcern.SAFE);
-				} catch(MongoException.DuplicateKey e) {
-					log.info("Dup val in subject: " + subject); 
+				subDoc.put(Constants.FIELD_HASH_VALUE, subjectDigestValue);
+				subDoc.put(Constants.FIELD_STR_VALUE, keyStr);
+				docs.add(subDoc);
+				while(values.hasNext()) {
+					Text predObjText = values.next();
+					String[] predObj = predObjText.toString().split(
+							Constants.REGEX_DELIMITER);
+					for(String term : predObj) {
+						String termDigestValue = 
+								Util.generateMessageDigest(term);
+						BasicDBObject termDoc = new BasicDBObject();
+						termDoc.put(Constants.FIELD_HASH_VALUE, 
+								termDigestValue);
+						termDoc.put(Constants.FIELD_STR_VALUE, term);
+						docs.add(termDoc);
+					}
 				}
-				
-				log.info("Processing triple with subject: " + subject);
-				
-				String predicate = triple.getPredicate().toString();
-				String hashPred = Util.generateMessageDigest(predicate);
-				boolean isType = false;
-				if(predicate.equals("<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>") ||
-						   predicate.equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#type") ||
-						   predicate.equals("rdf:type"))
-					isType = true;
-				BasicDBObject subPred = new BasicDBObject();
-				subPred.put(Constants.FIELD_STR_VALUE, predicate);
-				subPred.put(Constants.FIELD_HASH_VALUE, hashPred);
-				try {
-					eidValCollection.insert(subPred, WriteConcern.SAFE);
-				} catch(MongoException.DuplicateKey e) { 
-					log.info("Dup val in predicate: " + predicate); 
-				}
-				
-				String object = triple.getObject().toString();
-				String hashObj = Util.generateMessageDigest(object);
-				BasicDBObject subObj = new BasicDBObject();
-				subObj.put(Constants.FIELD_STR_VALUE, object);
-				subObj.put(Constants.FIELD_HASH_VALUE, hashObj);
-				subObj.put("isType", isType);
-				try {
-					idValCollection.insert(subObj, WriteConcern.SAFE);
-				} catch(MongoException.DuplicateKey e) { 
-					log.info("Dup val in obj: " + object); 
-				}
-			}
-			catch(Exception e) {
-				log.error("Exception in Map: " + e.getMessage());
+				idValCollection.insert(docs);
+				docs.clear();
+			}catch (Exception e) {
 				e.printStackTrace();
 			}
-			reporter.progress();
-/*			
-			if(tokens[Constants.POSITION_PREDICATE].equals(Constants.PREDICATE_TYPE))
-				output.collect(new Text(tokens[Constants.POSITION_OBJECT]), 
-								new Text(tokens[Constants.POSITION_SUBJECT] + 
-										Constants.OUTPUT_DELIMITER + 
-										Constants.PREDICATE_TYPE));
-			else
-				output.collect(new Text(tokens[Constants.POSITION_SUBJECT]), 
-						new Text(tokens[Constants.POSITION_OBJECT]));
-*/						
 		}
 		
 		@Override
 		public void close() throws IOException {
 			mongo.close();
-			model.close();
 		}
 	}
 	
 	@Override
 	public int run(String[] args) throws Exception {
-		if(args.length != 2) {
-			String message = "Incorrect arguments -- requires 2 argument.\n\t " +
-			"1) directory containing N-triples \n\t" +
-			"2) output directory path ";
+		if(args.length != 1) {
+			String message = "Incorrect arguments -- requires 1 argument.\n\t " +
+			"1) directory containing N-triples. " +
+			"Also check the value of mongos in property file";
 			throw new Exception(message);
 		}
 
-		String triples = args[0];
-		String outputDir = args[1];
-		
-		Path outputPath = new Path(outputDir);
-		Configuration fconf = new Configuration();
-		FileSystem fs = FileSystem.get(fconf);
-
-		if (fs.exists(outputPath)) {
-			fs.delete(outputPath, true);
-		}
-			
+		String triples = args[0];		
 		JobConf jobConf = new JobConf(this.getClass());
 		jobConf.setJobName("HashGeneratorMR");
 		
 		PropertyFileHandler propertyFileHandler = PropertyFileHandler.getInstance();
-		int mongosCount = propertyFileHandler.getMongosCount();
-		List<HostInfo> mongosList = propertyFileHandler.getAllMongoRouters();
-		jobConf.setInt("mongosCount", mongosCount);
-		for(int i=0; i<mongosList.size(); i++)
-			jobConf.set("mongos" + i, mongosList.get(i).toString());
+		jobConf.set("mongo.router", propertyFileHandler.getMongoRouter());
 		
 		log.info("task timeout: " + jobConf.get("mapreduce.task.timeout"));
 		// default is 10 mins, setting it to 30 mins
-		jobConf.setLong("mapreduce.task.timeout", 1800000);
-		
-		Path pOutput = new Path(outputDir);
-		
-		FileOutputFormat.setOutputPath(jobConf, pOutput);
+//		jobConf.setLong("mapreduce.task.timeout", 1800000);
 		
 		jobConf.setInputFormat(KeyValueTextInputFormat.class);
 		jobConf.setOutputFormat(TextOutputFormat.class);	
@@ -231,17 +161,14 @@ public class HashGeneratorMR extends Configured implements Tool {
 		FileInputFormat.setInputPaths(jobConf, new Path(triples));
 
 		jobConf.setMapperClass(Map.class);
-		jobConf.setNumReduceTasks(0);
-//		jobConf.setReducerClass(Reduce.class);
+//		jobConf.setNumReduceTasks(0);
+		jobConf.setReducerClass(Reduce.class);
 		
 		jobConf.setMapOutputKeyClass(Text.class);
-		jobConf.setMapOutputValueClass(Text.class);
-		
+		jobConf.setMapOutputValueClass(Text.class);		
 		RunningJob job = JobClient.runJob(jobConf);
-
 		if (!job.isSuccessful())
-			System.out.println("Hadoop Job Failed");
-		
+			System.out.println("Hadoop Job Failed");		
 		return 0;
 	}
 	
