24bdec643b44bbb4c28ed922f462b86cb7735c0e
Raghava <raghava@knoesis.org>
2014-04-29 00:48:59 -0400

    modified content to generate hash of subject/predicat/object and saves that to a file on HDFS.

--- src/dsparq/load/HashGeneratorMR.java
+++ src/dsparq/load/HashGeneratorMR.java
@@ -2,15 +2,17 @@ package dsparq.load;
 
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.KeyValueTextInputFormat;
@@ -24,15 +26,9 @@ import org.apache.hadoop.mapred.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
+import org.semanticweb.yars.nx.Literal;
 import org.semanticweb.yars.nx.Node;
 import org.semanticweb.yars.nx.parser.NxParser;
-
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
 
 import dsparq.misc.Constants;
 import dsparq.misc.PropertyFileHandler;
@@ -50,11 +46,11 @@ public class HashGeneratorMR extends Configured implements Tool {
 	private static final Logger log = Logger.getLogger(HashGeneratorMR.class); 
 	
 	private static class Map extends MapReduceBase implements 
-			Mapper<Text, Text, Text, Text> {
+			Mapper<Text, Text, Text, LongWritable> {
 		
 		@Override
 		public void map(Text key, Text value, 
-				OutputCollector<Text, Text> output,
+				OutputCollector<Text, LongWritable> output,
 				Reporter reporter) throws IOException {
 			
 			// expected input format: subject predicate object
@@ -66,89 +62,94 @@ public class HashGeneratorMR extends Configured implements Tool {
 			strBuilder.append(nodes[1].toString()).
 						append(Constants.TRIPLE_TERM_DELIMITER).
 						append(nodes[2].toString());
-			//group by subject
-			output.collect(new Text(nodes[0].toString()), 
-					new Text(strBuilder.toString()));
+			output.collect(new Text(nodes[0].toString()), new LongWritable(1)); 
+			output.collect(new Text(nodes[1].toString()), new LongWritable(1)); 
+			if(nodes[1].toString().equals(
+					"http://www.w3.org/1999/02/22-rdf-syntax-ns#type"))
+				output.collect(new Text(nodes[2].toString()), 
+						new LongWritable(-1)); 
+			else {
+				if(nodes[2] instanceof Literal) {
+					//make this in the same form as Jena sees it.
+					//Eg: "Journal 1 (1940)"^^http://www.w3.org/2001/XMLSchema#string
+					Literal literal = (Literal) nodes[2];
+					StringBuilder sb = new StringBuilder();
+					sb.append("\"").append(literal.getData()).append("\"");
+					sb.append("^^").append(literal.getDatatype().toString());
+					output.collect(new Text(sb.toString()), 
+							new LongWritable(1));
+				}
+				else
+					output.collect(new Text(nodes[2].toString()), 
+							new LongWritable(1));
+			}
+			reader.close();
 		}
 	}
 	
 	private static class Reduce extends MapReduceBase implements
-	Reducer<Text, Text, Text, Text> {
-		
-		// read Vertex ID of subject from DB.
-				private Mongo mongo;
-				private DBCollection idValCollection;
-
-				@Override
-				public void configure(JobConf jobConf) {
-					log.info("Configuring Mongo...");
-					try {
-						String[] hostPort = jobConf.get("mongo.router").split(":");
-						mongo = new MongoClient(hostPort[0], 
-								Integer.parseInt(hostPort[1]));
-						DB db = mongo.getDB(Constants.MONGO_RDF_DB);
-						idValCollection = db.getCollection(
-								Constants.MONGO_IDVAL_COLLECTION);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
+	Reducer<Text, LongWritable, Text, Text> {
 		
 		@Override
-		public void reduce(Text key, Iterator<Text> values,
-				OutputCollector<Text, Text> output, Reporter reporter) {
+		public void reduce(Text key, Iterator<LongWritable> values,
+				OutputCollector<Text, Text> output, 
+				Reporter reporter) {
 			try {
-				List<DBObject> docs = new ArrayList<DBObject>();
 				String keyStr = key.toString();
-				String subjectDigestValue = Util.generateMessageDigest(keyStr);
-				BasicDBObject subDoc = new BasicDBObject();
-				subDoc.put(Constants.FIELD_HASH_VALUE, subjectDigestValue);
-				subDoc.put(Constants.FIELD_STR_VALUE, keyStr);
-				docs.add(subDoc);
+				String digestValue = Util.generateMessageDigest(keyStr);
+				int typeID = -1;
 				while(values.hasNext()) {
-					Text predObjText = values.next();
-					String[] predObj = predObjText.toString().split(
-							Constants.REGEX_DELIMITER);
-					for(String term : predObj) {
-						String termDigestValue = 
-								Util.generateMessageDigest(term);
-						BasicDBObject termDoc = new BasicDBObject();
-						termDoc.put(Constants.FIELD_HASH_VALUE, 
-								termDigestValue);
-						termDoc.put(Constants.FIELD_STR_VALUE, term);
-						docs.add(termDoc);
+					if(values.next().get() == 1) {
+						typeID = 1;
+						break;
 					}
 				}
-				idValCollection.insert(docs);
-				docs.clear();
+				StringBuilder sb = new StringBuilder().append(digestValue).
+						append(Constants.TRIPLE_TERM_DELIMITER).append(typeID);
+				output.collect(new Text(sb.toString()), key);
+/*				
+				BasicBSONObject doc = new BasicBSONObject();
+				doc.put(Constants.FIELD_HASH_VALUE, digestValue);
+				doc.put(Constants.FIELD_STR_VALUE, keyStr);
+				doc.put(Constants.FIELD_TYPEID, typeID);
+				output.collect(new BSONWritable(doc), null);
+*/				
 			}catch (Exception e) {
 				e.printStackTrace();
 			}
-		}
-		
-		@Override
-		public void close() throws IOException {
-			mongo.close();
 		}
 	}
 	
 	@Override
 	public int run(String[] args) throws Exception {
-		if(args.length != 1) {
-			String message = "Incorrect arguments -- requires 1 argument.\n\t " +
+		if(args.length != 2) {
+			String message = "Incorrect arguments -- requires 2 argument.\n\t " +
 			"1) directory containing N-triples. " +
-			"Also check the value of mongos in property file";
+			"2) Output Directory";
 			throw new Exception(message);
 		}
 
-		String triples = args[0];		
+		String triples = args[0];
+		String outputDir = args[1];
+		Path outputPath = new Path(outputDir);
+		Configuration fconf = new Configuration();
+		FileSystem fs = FileSystem.get(fconf);
+
+		if (fs.exists(outputPath)) {
+			fs.delete(outputPath, true);
+		}
 		JobConf jobConf = new JobConf(this.getClass());
 		jobConf.setJobName("HashGeneratorMR");
 		
-		PropertyFileHandler propertyFileHandler = PropertyFileHandler.getInstance();
-		jobConf.set("mongo.router", propertyFileHandler.getMongoRouter());
+//		Configuration conf = new Configuration();
+//       MongoConfig config = new MongoConfig(jobConf);
+//        setConf(jobConf);
 		
-		log.info("task timeout: " + jobConf.get("mapreduce.task.timeout"));
+//		PropertyFileHandler propertyFileHandler = PropertyFileHandler.getInstance();
+//		String mongoRouter = propertyFileHandler.getMongoRouter();
+		
+//		jobConf.set("mongo.router", propertyFileHandler.getMongoRouter());
+		
 		// default is 10 mins, setting it to 30 mins
 //		jobConf.setLong("mapreduce.task.timeout", 1800000);
 		
@@ -159,20 +160,34 @@ public class HashGeneratorMR extends Configured implements Tool {
 		jobConf.setOutputValueClass(Text.class);
 		
 		FileInputFormat.setInputPaths(jobConf, new Path(triples));
+		FileOutputFormat.setOutputPath(jobConf, outputPath);
 
+		PropertyFileHandler propertyFileHandler = PropertyFileHandler.getInstance();
+		int numNodes = propertyFileHandler.getShardCount();
+		int numReducers = (int)Math.ceil(0.95 * numNodes * 2);
+		log.info("Number of reducers: " + numReducers);
+		
 		jobConf.setMapperClass(Map.class);
-//		jobConf.setNumReduceTasks(0);
 		jobConf.setReducerClass(Reduce.class);
+		jobConf.setNumReduceTasks(numReducers);
 		
 		jobConf.setMapOutputKeyClass(Text.class);
-		jobConf.setMapOutputValueClass(Text.class);		
+		jobConf.setMapOutputValueClass(LongWritable.class);	
+		
 		RunningJob job = JobClient.runJob(jobConf);
 		if (!job.isSuccessful())
-			System.out.println("Hadoop Job Failed");		
+			System.out.println("Hadoop Job Failed");	
+		
+		
+//		config.setInputFormat(KeyValueTextInputFormat.class);
+//		config.setOutputFormat(MongoOutputFormat.class);
+//		config.setOutputURI("mongodb://" + mongoRouter+"testdb.sample");
+//		config.setMapper(Map.class);
+		
 		return 0;
 	}
 	
 	public static void main(String[] args) throws Exception {
-		ToolRunner.run(new Configuration(), new HashGeneratorMR(), args);
+		ToolRunner.run(new HashGeneratorMR(), args);
 	}
 }
