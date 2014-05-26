05f3d1bac62e67bcc30cfc5d4609a815ea7b7f2f
Raghava <raghava@knoesis.org>
2014-05-26 11:19:08 -0400

    fixed a bug: need not use parseInt() on startNum, but should use it on ip.

--- src/dsparq/load/IDGenerator.java
+++ src/dsparq/load/IDGenerator.java
@@ -20,7 +20,7 @@ import dsparq.util.Util;
  * @author Raghava
  */
 public class IDGenerator {
-
+	
 	public void generateIDs() throws Exception {
 		PropertyFileHandler propertyFileHandler = 
 				PropertyFileHandler.getInstance();
@@ -40,16 +40,17 @@ public class IDGenerator {
 					"var statsCollection = db.getCollection('" + 
 						Constants.MONGO_STATS_COLLECTION + "'); " +
 					"var statsDoc = statsCollection.findOne(); " + //there is only 1 document
-					"var startNum = parseInt(statsDoc['" + 
-						Constants.TOTAL_DOCS + "']); " +
+					"var startNum = statsDoc['" + 
+						Constants.TOTAL_DOCS + "']; " +
 					"var idValsCollection = db.getCollection('" + 
 						Constants.MONGO_IDVAL_COLLECTION + "'); " +
 					"var cursor = idValsCollection.find({}, {" + 
 						Constants.FIELD_HASH_VALUE + ":1, " + 
 						Constants.FIELD_TYPEID + ":1}); " +
+					"var ipNum = parseInt(ip); " +
 					"while(cursor.hasNext()) { " +
 						"var doc = cursor.next(); " +
-						"var tid = parseInt(doc['" + Constants.FIELD_TYPEID + "']); " +
+						"var tid = doc['" + Constants.FIELD_TYPEID + "']; " +
 						"if(tid == 1) { " +
 							"idValsCollection.update({" + 
 								Constants.FIELD_HASH_VALUE + ": doc['" + 
@@ -63,19 +64,21 @@ public class IDGenerator {
 								Constants.FIELD_HASH_VALUE + ": doc['" + 
 								Constants.FIELD_HASH_VALUE + "']}, " +
 										"{$set : {" + Constants.FIELD_NUMID + 
-										": ip}}); " +
-							"ip = ip + 1; " +
+										": ipNum}}); " +
+							"ipNum = ipNum + 1; " +
 						"} " +
 					"} " +
-					"return {totalTerms : startNum} " +	
+					"return {totalTerms : startNum}; " +	
 				"} ";
 		String ip = InetAddress.getLocalHost().getHostAddress();
 		String[] ipBlocks = ip.split("\\.");
 		StringBuilder flattenedIP = new StringBuilder().append(ipBlocks[0]).
 				append(ipBlocks[1]).append(ipBlocks[2]).append(ipBlocks[3]);
-//		System.out.println("Using IP: " + flattenedIP);
+		System.out.println("Using IP: " + flattenedIP);
+		System.out.println("Script: " + insertNumIDScript);
 		CommandResult result = localDB.doEval(insertNumIDScript, 
 				flattenedIP.toString());
+		System.out.println("Result: " + result.toString());
 		BasicDBObject resultDoc = (BasicDBObject) result.get("retval");
 		System.out.println("Total terms without types: " + 
 				(resultDoc.getInt("totalTerms")-1));
